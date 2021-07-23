42673beb8fb6830d515228106b9c457f3de641fd
eyckchuang <eyckchuang@liontravel.com>
2021-07-23 14:18:42 +0800

    add idea

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
+
 app = Flask(__name__)
 app.static_folder = 'static'
+
 
 @app.route('/home')
 def home():
@@ -17,9 +19,17 @@ def home():
 #     students = [('Anil',55),('Rajeev',40),('Leela',60),('Zuber',75),('John',30)]
 #     return render_template("langs.html",students=students)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        nm = request.form.get('name')
+        # nm = request.args.get('name')
+        # nm = request.args.get('name')
+        # nm = request.args.get('name')
+        return """<b>Name:</b>{}<br>""".format(nm)
+
+    return "get"
+
 
 @app.route('/login')
 def login():
@@ -27,31 +37,50 @@ def login():
 
 
 @app.route('/hello/<name>/<int:age>')
-def hello(name,age):
-    return render_template('hello.html',name=name,age=age)
+def hello(name, age):
+    return render_template('hello.html', name=name, age=age)
 
 
 @app.template_filter("isodd")
 def isodd(x):
-    if x%2==1:
-        result=True
+    if x % 2 == 1:
+        result = True
     else:
-        result=False
+        result = False
     return result
 
 
 @app.route('/<name>')
 def index(name):
-    return render_template('btn.html',name=name)
+    return render_template('btn.html', name=name)
 
 
 @app.route('/page')
 def page():
     name = request.args.get('name')
     age = request.args.get('age')
-    return render_template('page.html',name=name,age=age)
+    return render_template('page.html', name=name, age=age)
 
 
 @app.route('/form')
 def form():
-    return render_template('form.html')
+    return render_template('register.html')
+
+
+@app.route('/setcookie', methods=['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        user = request.form['name']
+        resp = make_response(render_template('setcookie.html'))
+        resp.set_cookie('userID', user)
+        return resp
+
+
+@app.route('/readcookie')
+def readcookie():
+    name = request.cookies.get('userID')
+    return render_template('readcookie.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run()
