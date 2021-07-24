67d3c10c7ae9631670542c58b1898be50e8314fd
eyckchuang <eyckchuang@liontravel.com>
2021-07-25 06:45:36 +0800

    page178

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,7 +1,29 @@
+import sqlite3
+
 from flask import Flask, render_template, request, make_response
+
+from flaskapp.encryption import encpwd
 
 app = Flask(__name__)
 app.static_folder = 'static'
+
+
+@app.route('/createtable')
+def createtable():
+    con = sqlite3.connect("mydata.db")
+    cur = con.cursor()
+    createqry = '''
+    create table if not exists Students (
+    Name string (20) not null,
+    Course string (20) ,
+    Gender string (20),
+    Mobile integer (10),
+    Username string (6) primary key not null,
+    Password text (8) not null
+    );
+    '''
+    cur.execute(createqry)
+    return 'ok'
 
 
 @app.route('/home')
@@ -28,7 +50,7 @@ def register():
         # nm = request.args.get('name')
         return """<b>Name:</b>{}<br>""".format(nm)
 
-    return "get"
+    return render_template("register.html")
 
 
 @app.route('/login')
@@ -80,6 +102,40 @@ def setcookie():
 def readcookie():
     name = request.cookies.get('userID')
     return render_template('readcookie.html', name=name)
+
+
+@app.route('/addrec', methods=['POST', 'GET'])
+def addrec():
+    if request.method == 'POST':
+        con = sqlite3.connect("mydata.db")
+        cur = con.cursor()
+        msg = ''
+        try:
+            nm = request.form['name']
+            gndr = request.form['gender']
+            course = request.form['course']
+            mob = request.form['mobile']
+            usr = request.form['user']
+            pw = request.form['pwd']
+            ins = "insert into Students values(?,?,?,?,?,?)"
+            cur.execute(ins, (nm, gndr, course, mob, usr, encpwd(pw)))
+            con.commit()
+            msg = "record successfully added"
+        except Exception as e:
+            con.rollback()
+            msg = "error in insert operation"
+        finally:
+            return render_template('result.html', msg=msg)
+
+
+@app.route('/list')
+def list():
+    con = sqlite3.connect("mydata.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("select * from Students;")
+    students = cur.fetchall()
+    return render_template("studentlist.html", students=students)
 
 
 if __name__ == '__main__':
